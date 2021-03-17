7038fd09a0b2fe7a99f3897621a35bd25c8f37e0
Michael Piazza <michael.piazza.mp@gmail.com>
2021-03-17 15:06:21 -0700

    Trending Playlists  (#271)
    * New hooks
    
    * Tile + skeleton fixes
    
    * Lineup fixes
    
    * Fix collection retrieve types
    
    * TrendingPlaylistPage + store
    
    * PR fixes

--- src/containers/App.js
+++ src/containers/App.js
@@ -57,7 +57,8 @@ import {
   APP_REDIRECT,
   TRACK_ID_PAGE,
   USER_ID_PAGE,
-  PLAYLIST_ID_PAGE
+  PLAYLIST_ID_PAGE,
+  TRENDING_PLAYLISTS_PAGE
 } from 'utils/route'
 import 'utils/redirect'
 import { isMobile, getClient } from 'utils/clientUtil'
@@ -148,6 +149,7 @@ import Notice from './notice/Notice'
 import SignOn from 'containers/sign-on/SignOn'
 import EnablePushNotificationsDrawer from './enable-push-notifications-drawer/EnablePushNotificationsDrawer'
 import { ThemeChangeMessage } from 'services/native-mobile-interface/theme'
+import TrendingPlaylistsPage from 'containers/trending-playlists/TrendingPlaylistPage'
 
 const MOBILE_BANNER_LOCAL_STORAGE_KEY = 'dismissMobileAppBanner'
 
@@ -569,6 +571,15 @@ class App extends Component {
                 render={() => (
                   <Discover
                     feedIsMain={false}
+                    containerRef={this.state.mainContent}
+                  />
+                )}
+              />
+              <Route
+                exact
+                path={TRENDING_PLAYLISTS_PAGE}
+                render={() => (
+                  <TrendingPlaylistsPage
                     containerRef={this.state.mainContent}
                   />
                 )}
